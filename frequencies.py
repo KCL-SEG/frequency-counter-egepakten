b1fab7adf9dd7d8ecdc31bd14cd380e738557f3f
Pakten, Kerem (k20057059) <kerem_ege.pakten@kcl.ac.uk>
2022-12-03 21:55:14 +0000

    Final_version_1

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,15 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
-
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        counter = 1
+        for search_item in items:
+            if item == search_item:
+                frequencies[item] = counter
+                counter+=1
     return frequencies
+
+
+#print(frequencies(['0', 4,4,'4','d','d','e',0,'a','d','4']))
